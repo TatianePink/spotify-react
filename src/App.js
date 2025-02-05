c94e9b6a0ee10ef29387f01754df2b4aff2d27e6
TatianePink <tpink@faeterj-petropolis.edu.br>
2025-02-04 21:17:20 -0300

    criaçao

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Hader/Header';
+import Cards from './Cards/Cards';
+import SideBar from './SideBar/SideBar';
+import Footer from './Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <Cards />
+     <Footer />
+    </>
+  );
+}
+
+export default App;
